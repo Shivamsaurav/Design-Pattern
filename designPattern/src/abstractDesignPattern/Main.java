4c4daf05d4deab44601cbfcd93a222f2361fb35f
SHIVAM <shivam@SHIVAMs-MacBook.local>
2025-02-08 11:16:00 +0530

    Design Patterns

---
+++ designPattern/src/abstractDesignPattern/Main.java
@@ -0,0 +1,10 @@
+package abstractDesignPattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ designPattern/src/adapterDesignPattern/Main.java
@@ -0,0 +1,10 @@
+package adapterDesignPattern;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
